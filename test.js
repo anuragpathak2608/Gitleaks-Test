68cbb6fba67d92ca74897ed66084d9ef9dfab597
Yogesh <yogesh.b@thoughtworks.com>
2022-08-22 20:19:52 +0530

    Test commit #11

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const password = "qwerty$asdfgh#zxcvbn";
 const amazon_url = "https://amazon.com";
 const username = "Yogesh";
+const fake_ak = "AKIAIO5FODNN7EXAMPLE";
+const fake_sk = "ABCDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE";
