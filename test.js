51a28e83908ea33b1fe169e228acc11219b6d08a
Yogesh <yogesh.b@thoughtworks.com>
2022-08-23 10:03:49 +0530

    Test commit #13

--- test.js
+++ test.js
@@ -5,3 +5,4 @@ const fake_ak = "AKIAIO5FODNN7EXAMPLE";
 const fake_sk = "ABCDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE";
 const accessKey = "AKIAIOSFODNN7EXAMPLE";
 const secretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+salePriceShouldReflectPriceMinusPromotions();
