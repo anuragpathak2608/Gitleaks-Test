3236a36e3b74e19e8916ffd93383576deef532d3
anuragpathak2608 <48289570+anuragpathak2608@users.noreply.github.com>
2022-08-29 13:05:47 +0530

    testfile created

---
+++ testfile.py
@@ -0,0 +1,8 @@
+const password = "qwerty$asdfgh#zxcvbn";
+const amazon_url = "https://amazon.com";
+const username = "Yogesh";
+const fake_ak = "AKIAIO5FODNN7EXAMPLE";
+const fake_sk = "ABCDEF+c2L7yXeGvUyrPgYsDnWRRC1AYEXAMPLE";
+const accessKey = "AKIAIOSFODNN7EXAMPLE";
+const secretKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+salePriceShouldReflectPriceMinusPromotions();
